82d39e1e89d404c0ae38c19d6a0fcb1aed9ee042
johnlazarodigital <johnlazarodigital@gmail.com>
2019-11-17 09:47:25 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Plugin Name: Health Tracker
+Plugin URI: https://johnlazaro.com/
+Description: Health tracker plugin for WordPress
+Version: 1.0
+Author: John Lazaro
+URI: https://johnlazaro.com/
+License: GPLv2 or later */
